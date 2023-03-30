9340ecd905b99eeab41b4f6fc1c5d775a3e58239
FuerzaDON <luis_fernando_chavez@hotmail.com>
2023-03-30 01:00:51 -0600

    Interview Javascript test added

---
+++ appInterviewTest.js
@@ -0,0 +1,10 @@
+console.log(1 == '1');
+console.log(false == '0');
+console.log(true == '1');
+console.log('1' == '01');
+console.log(10 == 5 + 5);
+console.log(3 + 4 + '5');
+console.log(false == '1');
+console.log(true == '0');
+
+
